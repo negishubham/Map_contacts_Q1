435137f694738c321c12aa9ba5c8a766aed80d52
shubham negi <shubhamnegi1990@gmail.com>
2017-08-27 23:29:57 +0530

    final

--- app/src/main/java/com/example/shubham/contact_map_1/MainActivity.java
+++ app/src/main/java/com/example/shubham/contact_map_1/MainActivity.java
@@ -1,54 +1,72 @@
 package com.example.shubham.contact_map_1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    String[][] word = new String[5][4];
+    int count =0;
 
-    EditText et_text;
-    Button b_read;
-    TextView tv_text;
-    String fileName = "contacts.txt"
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relative_layout);
+
+        TextView tv = (TextView) findViewById(R.id.text); //Find the view by its id
+        TextView tvp1 = (TextView) findViewById(R.id.p1);
+        TextView tvp2 = (TextView) findViewById(R.id.p2);
 
 
-        et_text = (EditText) findViewById(R.id.et_text);
-        b_read = (Button) findViewById(R.id.b_read);
-        tv_text = (TextView) findViewById(R.id.tv_text);
+        File sdcard = Environment.getExternalStorageDirectory(); //Find the directory for the SD Card using the API
 
-        b_read.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                tv_text.setText(readFile(fileName)));
+        File file = new File(sdcard, "contacts.txt");   //Get the text file
+
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();   //Read text from file
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = br.readLine()) != null) {
+
+                    word[count]= line.split("\\s");
+                    text.append(line);
+                    text.append('\n');
+
+                    Intent contactIntent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                    contactIntent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+                     contactIntent
+                            .putExtra(ContactsContract.Intents.Insert.NAME,word[count][0])
+                            .putExtra(ContactsContract.Intents.Insert.EMAIL, word[count][1])
+                            .putExtra(ContactsContract.Intents.Insert.SECONDARY_PHONE,word[count][2])
+                            .putExtra(ContactsContract.Intents.Insert.PHONE, word[count][3]);
+                            startActivityForResult(contactIntent, 1);
+                    count=count+1;
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Error reading file", Toast.LENGTH_SHORT).show();
             }
+            tv.setText(word[0][2]);   //Set the text
+            tvp1.setText(word[0][0]);
+            tvp2.setText(word[0][1]);
+        } else {
+            tv.setText("Sorry file doesn't exist");
         }
-
     }
-
-    public String readFile(String file){
-        String text ="";
-
-        try{
-            FileInputStream fis = openFileInput(file);
-            int size = fis.available();
-            byte[] buffer = new byte(size);
-            fis.read(buffer);
-            fis.close();
-            text = new String (buffer);
-        } catch (Exception e){
-            e.printStackTrace();
-            Toast.makeText(MainActivity.this,"Error reading file", Toast.LENGTH_SHORT).show();
-        }
-
-        return text;
-    }
-
 }
+
